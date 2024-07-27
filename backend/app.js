bcadd61ff847c8130a4b9fa512a5d9f095a57abb
Aayush701 <ayushghimire70111@gmail.com>
2024-07-27 15:23:49 +0545

    backend almost completed . email automation is left in backend

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import { connection } from "./database/connection.js";
 import { errorMiddleware } from "./middlewares/error.js";
 import fileUpload from "express-fileupload";
 import userRouter from "./routes/userRouter.js";
-
+import jobRouter from "./routes/jobRouter.js";
 const app = express();
 config({ path: "./config/config.env" });
 
@@ -28,6 +28,7 @@ app.use(
 );
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
 
 // newsLetterCron();
 
