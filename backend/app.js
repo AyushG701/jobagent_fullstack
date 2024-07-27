f94ae89fbafa4d86fd453cc07131caa61591c5f5
Aayush701 <ayushghimire70111@gmail.com>
2024-07-27 18:48:20 +0545

    email automation added from node-cron

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ import { errorMiddleware } from "./middlewares/error.js";
 import fileUpload from "express-fileupload";
 import userRouter from "./routes/userRouter.js";
 import jobRouter from "./routes/jobRouter.js";
+import applicationRouter from "./routes/applicationRouter.js";
+
 const app = express();
 config({ path: "./config/config.env" });
 
@@ -29,8 +31,9 @@ app.use(
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
 
-// newsLetterCron();
+newsLetterCron();
 
 connection();
 app.use(errorMiddleware);
