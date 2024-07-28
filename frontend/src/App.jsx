8fe7f293c2e836834eeac44d5c106096f3d5e207
Aayush701 <ayushghimire70111@gmail.com>
2024-07-28 13:44:12 +0545

    structuring and setup component completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import react from "react";
+import { Button } from "@/components/ui/button";
+
+import Navbar from "./segments/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
+import Dashboard from "./pages/Dashboard";
+import PostApplication from "./pages/PostApplication";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Footer from "./segments/Footer";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/post/application/:jobId"
+            element={<PostApplication />}
+          />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/segments/Navbar.jsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+// import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Menu } from "lucide-react";
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  // const { isAuthenticated } = useSelector((state) => state.user);
+  return (
+    <>
+      <header className="bg-white  ">
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="md:flex md:items-center md:gap-12">
+              <Link className="block text-teal-600" to="/">
+                <span className="sr-only">Home</span>
+                <img src="/logo.png" alt="logo" className="h-8" />
+              </Link>
+            </div>
+
+            <div className="hidden md:block">
+              <nav aria-label="Global">
+                <ul className="flex items-center gap-6 text-sm">
+                  <li>
+                    <Link
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      to="/"
+                      onClick={() => setShow(false)}
+                    >
+                      HOME
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      to="/jobs"
+                      onClick={() => setShow(false)}
+                    >
+                      JOBS
+                    </Link>
+                  </li>
+                  {isAuthenticated ? (
+                    <li>
+                      <Link
+                        className="text-gray-500 transition hover:text-gray-500/75"
+                        to="/dashboard"
+                        onClick={() => setShow(false)}
+                      >
+                        DASHBOARD
+                      </Link>
+                    </li>
+                  ) : (
+                    <li>
+                      <Link
+                        className="text-gray-500 transition hover:text-gray-500/75"
+                        to="/login"
+                        onClick={() => setShow(false)}
+                      >
+                        LOGIN
+                      </Link>
+                    </li>
+                  )}
+                </ul>
+              </nav>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <div className="sm:flex sm:gap-4">
+                <Link
+                  className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+                  to="/login"
+                  onClick={() => setShow(false)}
+                >
+                  Login
+                </Link>
+
+                <div className="hidden sm:flex">
+                  <Link
+                    className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+                    to="/register"
+                    onClick={() => setShow(false)}
+                  >
+                    Register
+                  </Link>
+                </div>
+              </div>
+
+              <div className="block md:hidden">
+                <button
+                  className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
+                  onClick={() => setShow(!show)}
+                >
+                  <Menu className="h-5 w-5" />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {show && (
+            <div className="block md:hidden">
+              <nav aria-label="Global">
+                <ul className="flex flex-col items-start gap-4 text-sm">
+                  <li>
+                    <Link
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      to="/"
+                      onClick={() => setShow(false)}
+                    >
+                      HOME
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      to="/jobs"
+                      onClick={() => setShow(false)}
+                    >
+                      JOBS
+                    </Link>
+                  </li>
+                  {isAuthenticated ? (
+                    <li>
+                      <Link
+                        className="text-gray-500 transition hover:text-gray-500/75"
+                        to="/dashboard"
+                        onClick={() => setShow(false)}
+                      >
+                        DASHBOARD
+                      </Link>
+                    </li>
+                  ) : (
+                    <li>
+                      <Link
+                        className="text-gray-500 transition hover:text-gray-500/75"
+                        to="/login"
+                        onClick={() => setShow(false)}
+                      >
+                        LOGIN
+                      </Link>
+                    </li>
+                  )}
+                </ul>
+              </nav>
+            </div>
+          )}
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
